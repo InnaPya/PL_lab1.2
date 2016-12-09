9a760290be122a73df1b984ffc75ab911df533a9
InnaPya <youwarkee006@gmail.com>
2016-12-09 20:16:20 +0700

    dddd

---
+++ lab1.js
@@ -0,0 +1,32 @@
+var request = require('request');
+var url = 'http://www.mosigra.ru';
+//var url = 'http://www.csd.tsu.ru';
+var b = [];
+var old = [];
+var c = 0;
+var depth = 3;
+
+function rec(u, c) {
+	c += 1;
+	request(u, function(err,res,body) {
+		if (err) console.log(err);
+		if (body != undefined) {
+			var a = body.match(/[A-Za-z\d][\w\.-]+@[\w\.-]+\.[A-Za-z\d-]+[a-zA-Z\d]/ig);
+			for (var i in a) {
+				if (!(b.includes(a[i]))) {b.push(a[i]); console.log(a[i]);}
+			}
+			var l = body.match(/<a href="(\/[-+\w:\/#@$.]*)/ig);
+			for (var i in l) {
+				if (!(old.includes(l[i]))) l.splice(i, 1);
+				else old.push(l[i]);
+			}
+			if (c < depth) {
+				for (var i = 0; i < 30; i++) {
+					v = l[i].substring(9);
+					rec(u+v, c);
+				}
+			}
+		}		
+	});
+}
+rec(url, c);
